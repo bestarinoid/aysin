6a285f4d3c64eca001a04168b12bc7c7bf627922
1002058460 <90251152+1002058460@users.noreply.github.com>
2021-09-27 09:17:04 +0700

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Second from './Second';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Second />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
